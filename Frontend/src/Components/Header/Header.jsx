cb25f19cb2434d4b09c8ef1a4b9c264b221a0c42
Dheerendra Vikram Dixit <dheerendradixit321@gmail.com>
2025-04-26 18:51:36 +0530

    adding backend

--- Frontend/src/Components/Header/Header.jsx
+++ Frontend/src/Components/Header/Header.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import './Header.css'
+import headerImg from '../../assets/header_img.png' // Import the image properly
+
 const Header = () => {
   return (
-    <div className='header'>
+    <div className='header' style={{ backgroundImage: `url(${headerImg})` }}>
       <div className="header-contents">
         <h2>Order your favorite food here</h2>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores dolorem dolores minus nam quia accusantium reiciendis aperiam porro commodi. A autem praesentium libero, corrupti soluta ratione quis esse dolorum temporibus minima voluptatibus fugit inventore! Quo error perspiciatis consequatur qui aliquam.</p>
